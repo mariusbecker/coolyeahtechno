407cb888b2a17ac118eb9d1c14ee67b8d4131a0d
Marius Becker <marius@Mariuss-MacBook-Pro.local>
2018-01-20 13:29:14 +0100

    checkbox-drawing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+
+$(document).ready(function () {
+
+	const firstCheckbox = $('.my-checkbox');
+  const container = $('.checkbox-container');
+
+
+  const checkboxesX = container.width() / firstCheckbox.width();
+  const checkboxesY = container.height() / firstCheckbox.height();
+  const checkboxesTotal = checkboxesX * checkboxesY - 1;
+  const newCheckbox = '<input type="checkbox">';
+
+  for (let i = 0; i < checkboxesTotal; i = i + 1) {
+  	container.append(newCheckbox);
+  }
+
+});
